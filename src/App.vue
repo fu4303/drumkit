47b45f5382aa3e1886a7baaa3544c3d935d34201
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2018-08-17 11:46:18 -0400

    features/sound plays when user presses keyboard

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,116 @@
 <template>
   <div id="app">
     <div id="drum-machine">
-      <section id="display">
-        <h1>DrumKit</h1>
-      </section>
+      <h1>DrumKit</h1>
+      <span id="display"></span>
       <section id="pad-section">
-        <article class='drum-pad drum-pad--1' :style="{pulse}">
-          <audio id="">
-            <source>
-            <source>
-            <p>Your browser doesn't support HTML5 audio.</p>
+        <article class='drum-pad drum-pad--1'
+          data-key="81"
+        >
+          <audio
+            id="Q"
+            class="clip"
+            data-key="81"
+            src="/samples/kick-tron.wav">
           </audio>
+          <kbd>Q</kbd>
+          <span class="audio-sound">Kick Tron</span>
         </article>
-        <article class='drum-pad drum-pad--2'>
-          <audio id="">
-            <source>
-            <source>
-            <p>Your browser doesn't support HTML5 audio.</p>
+        <article class='drum-pad drum-pad--2'
+          data-key="87"
+        >
+          <audio
+            id="W"
+            class="clip"
+            src="/samples/perc-tribal.wav"
+            data-key="87">
           </audio>
+          <kbd></kbd>
+          <span class="audio-sound"></span>
         </article>
-        <article class='drum-pad drum-pad--3'>
-          <audio id="">
-            <source>
-            <source>
-            <p>Your browser doesn't support HTML5 audio.</p>
+        <article class='drum-pad drum-pad--3'
+          data-key="69"
+        >
+          <audio
+            id="E"
+            class="clip"
+            src="/samples/openhat-acoustic01.wav"
+            data-key="69">
           </audio>
+          <kbd></kbd>
+          <span class="audio-sound"></span>
         </article>
-        <article class='drum-pad drum-pad--4'>
-          <audio id="">
-            <source>
-            <source>
-            <p>Your browser doesn't support HTML5 audio.</p>
+        <article class='drum-pad drum-pad--4'
+          data-key="65"
+        >
+          <audio
+            id="A"
+            class="clip"
+            src="/samples/snare-vinyl02.wav"
+            data-key="65">
           </audio>
+          <kbd></kbd>
+          <span class="audio-sound"></span>
         </article>
-        <article class='drum-pad drum-pad--5'>
-          <audio id="">
-            <source>
-            <source>
-            <p>Your browser doesn't support HTML5 audio.</p>
+        <article class='drum-pad drum-pad--5'
+          data-key="83"
+        >
+          <audio
+            id="S"
+            class="clip"
+            src="/samples/tom-fm.wav"
+            data-key="83">
           </audio>
+          <kbd></kbd>
+          <span class="audio-sound"></span>
         </article>
-        <article class='drum-pad drum-pad--6'>
-          <audio id="">
-            <source>
-            <source>
-            <p>Your browser doesn't support HTML5 audio.</p>
+        <article class='drum-pad drum-pad--6'
+          data-key="68"
+        >
+          <audio
+            id="D"
+            class="clip"
+            src="/samples/tom-rototom.wav"
+            data-key="68">
           </audio>
+          <kbd></kbd>
+          <span class="audio-sound"></span>
         </article>
-        <article class='drum-pad drum-pad--7'>
-          <audio id="">
-            <source>
-            <source>
-            <p>Your browser doesn't support HTML5 audio.</p>
+        <article class='drum-pad drum-pad--7'
+          data-key="90"
+        >
+          <audio
+            id="Z"
+            class="clip"
+            src="/samples/clap-crushed.wav"
+            data-key="90">
           </audio>
+          <kbd></kbd>
+          <span class="audio-sound"></span>
         </article>
-        <article class='drum-pad drum-pad--8'>
-          <audio id="">
-            <source>
-            <source>
-            <p>Your browser doesn't support HTML5 audio.</p>
+        <article class='drum-pad drum-pad--8'
+          data-key="88"
+        >
+          <audio
+            id="X"
+            class="clip"
+            src="/samples/kick-cultivator.wav"
+            data-key="88">
           </audio>
+          <kbd></kbd>
+          <span class="audio-sound"></span>
         </article>
-        <article class='drum-pad drum-pad--9'>
-          <audio id="">
-            <source>
-            <source>
-            <p>Your browser doesn't support HTML5 audio.</p>
+        <article class='drum-pad drum-pad--9'
+          data-key="67"
+        >
+          <audio
+            id="C"
+            class="clip"
+            src="/samples/kick-newwave.wav"
+            data-key="67">
           </audio>
+          <kbd></kbd>
+          <span class="audio-sound"></span>
         </article>
       </section>
     </div>
@@ -82,12 +126,31 @@ export default {
     HelloWorld
   },
   methods: {
-    addEffect() {
-      var vm = this;
-      vm.classList.add('pulse')
+    playAudio(e) {
+      //Instantiates audio variable to bind to the data-key
+      const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+      const key = document.querySelector(`article[data-key="${e.keyCode}"]`)
+
+
+      //If the audio isn't found, return
+      if(!audio) return;
+      key.classList.add('pulse');
+      //Set startTime at 0 & play
+      audio.startTime = 0;
+      audio.play();
+    },
+    removeTransition(e) {
+      if(e.propertyName !== 'transform') return;
+      e.target.classList.remove('pulse');
     }
   },
-  watch: {
+  created () {
+    let keys = document.querySelectorAll('.drum-pad');
+    keys.forEach(key => key.addEventListener('transitionend', this.removeTransition))
+    window.addEventListener('keyup', this.playAudio)
+  },
+  beforeDestory () {
+    //Do something
 
   }
 }
